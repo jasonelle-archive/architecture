60694f83faab7523e66d251737354a5a04a6524d
Camilo <camilo@ninjas.cl>
2021-06-13 20:34:27 -0400

    first adr

---
+++ bin/adr-index.js
@@ -0,0 +1,21 @@
+// License: MIT
+// By: CLSource <camilo@ninjas.cl>
+// This file generates a new README.adoc with all the ADR included
+
+// Deps
+const path = require('path');
+
+const {Mustache, helpers, vars} = require("./lib");
+const {workdir, indexTemplate} = vars;
+
+// Main
+(() => {
+  const files = helpers.decisions();
+  const includes = files.map(item => `\ninclude::adrs/${item}[]`).join("\n");
+
+  const indexContent = "// auto-generated. modify docs/templates/index.template.adoc and recompile instead.\n" +
+                        Mustache.render(indexTemplate, {includes});
+
+  helpers.save(path.resolve(workdir, 'README.adoc'), indexContent);
+})();
+
---
+++ bin/adr-new.js
@@ -0,0 +1,73 @@
+// License: MIT
+// By: CLSource <camilo@ninjas.cl>
+// This file renders a new adr file
+// Some code based on https://raw.githubusercontent.com/keremciu/adr-tool/main/src/commands/create.ts
+
+// Deps
+const path = require('path');
+const {Mustache, helpers, vars} = require("./lib");
+
+// Clean the args
+const args = [...process.argv];
+args.shift();
+args.shift();
+
+const title = args.join(" ");
+const cleanTitle = title.toLowerCase().trim()
+    .replace(/[\s_-]+/g, '-') // swap any length of whitespace, underscore, hyphen characters with a single _
+    .replace(/^-+|-+$/g, '') // remove leading, trailing -
+    .replace(/，/g, '')
+    .replace(/。/g, '')
+    .replace(/ /g, '-')
+    .replace(/\?/g, '-')
+    .replace(/#/g, '')
+    .replace(/:/g, '')
+    .replace(/# /g, '');
+
+// Helper functions
+
+const newFile = () => {
+
+  const getLatestIndex = () => {
+    const decisionFiles = helpers.decisions();
+
+    if (!(decisionFiles && decisionFiles.length > 0)) {
+      return 0;
+    }
+
+    return decisionFiles.length;
+  };
+
+  const {adrdir, extension} = vars;
+
+  const newIndex = getLatestIndex() + 1;
+  const fileIndex = ("000000" + newIndex).substr(3);
+  const filename = fileIndex + "-" + cleanTitle;
+  const filePath = path.resolve(adrdir, `${filename}.${extension}`);
+
+  const date = new Date();
+  const fileDate = date.toISOString();
+
+  return {
+    index: fileIndex,
+    fileIndex,
+    name: filename,
+    path:filePath,
+    date: fileDate,
+    dateLocale: date.toLocaleString(),
+    extension,
+    title,
+    cleanTitle
+  };
+};
+
+// Main
+(() => {
+  const {template} = vars;
+  const file = newFile();
+
+  const content = Mustache.render(template, file);
+  helpers.save(file.path, content);
+})();
+
+
